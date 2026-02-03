fe7b12b6346dbfbada3aab561efe38b88285e57d
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-02-03 18:39:38 +0530

    some update

--- simplehttpexperiment/src/ws.js
+++ simplehttpexperiment/src/ws.js
@@ -2,7 +2,17 @@ import { WebSocketServer } from "ws"
 
 export const initws = (server) => {
     const wss = new WebSocketServer({server})
-    wss.on("connection",(ws)=>{
-        console.log("connected to ws")
+
+wss.on("connection", (socket)=>{
+    socket.on("error", console.error)
+
+    socket.on("message", function message(data, isBinary){
+        wss.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data,{binary : isBinary})
+            }
+        })
     })
+    socket.send('Hello! Message From Server!!');
+})
 }
